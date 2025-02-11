d2b9bddf79794a640d6bb680488d8810f5ac8641
enrique <enriqueonaga@gmail.com>
2025-02-10 21:41:06 -0300

    Melhore o carrossel de cards

--- frontend/src/app/public/pages/home/home.component.ts
+++ frontend/src/app/public/pages/home/home.component.ts
@@ -1,6 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
-import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -12,15 +10,15 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.cards = [
-      { title: 'Card 1', description: 'Descrição do Card 1' },
-      { title: 'Card 2', description: 'Descrição do Card 2' },
-      { title: 'Card 3', description: 'Descrição do Card 3' },
-      { title: 'Card 4', description: 'Descrição do Card 4' },
-      { title: 'Card 5', description: 'Descrição do Card 5' },
-      { title: 'Card 6', description: 'Descrição do Card 6' },
-      { title: 'Card 7', description: 'Descrição do Card 7' },
-      { title: 'Card 8', description: 'Descrição do Card 8' },
-      { title: 'Card 9', description: 'Descrição do Card 9' }
+      { title: 'Comida', description: 'Sua comida favorita esperando por você.', image: '../../../../assets/undraw_breakfast_rgx5.svg', link: 'http://localhost:4200/' },
+      { title: 'Tecnologia', description: 'Fique ligado nas novas tecnologias do mercado.', image: '../../../../assets/undraw_mobile-images_b202.svg', link: 'http://localhost:4200/' },
+      { title: 'Moda', description: 'Acessórios e roupas para realçar o seu estilo.', image: '../../../../assets/undraw_window-shopping_9l2k.svg', link: 'http://localhost:4200/' },
+      { title: 'Móveis', description: 'Móveis para o lar que você merece.', image: '../../../../assets/undraw_special-event_hv54.svg', link: 'http://localhost:4200/' },
+      { title: 'Limpeza', description: 'Produtos de limpeza para uma ambiente limpo e agradável.', image: '../../../../assets/undraw_clean-up_af4s.svg', link: 'http://localhost:4200/' },
+      { title: 'Brinquedo', description: 'Não precisa ser dia das crianças para presentear uma criança.', image: '../../../../assets/undraw_toy-car_ugyu.svg', link: 'http://localhost:4200/' },
+      { title: 'Card 7', description: 'Descrição do Card 7', image: '../../../../assets/undraw_breakfast_rgx5.svg', link: 'http://localhost:4200/' },
+      { title: 'Card 8', description: 'Descrição do Card 8', image: '../../../../assets/undraw_breakfast_rgx5.svg', link: 'http://localhost:4200/' },
+      { title: 'Card 9', description: 'Descrição do Card 9', image: '../../../../assets/undraw_breakfast_rgx5.svg', link: 'http://localhost:4200/' }
     ];
   }
 }
