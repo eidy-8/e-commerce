e107ef4c44f5f0f8d34cbbe86eb6df621830f3a6
enrique <enriqueonaga@gmail.com>
2025-02-22 18:29:29 -0300

    corrija o erro do deployment

---
+++ frontend/dist/frontend/browser/chunk-6ZIQNJSJ.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-FIAK27LI.js";import"./chunk-6DTLE22S.js";export{a as PublicModule};
---
+++ frontend/dist/frontend/browser/chunk-BHKJJAA2.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-KA3IENRG.js";import"./chunk-6DTLE22S.js";export{a as PrivateModule};
