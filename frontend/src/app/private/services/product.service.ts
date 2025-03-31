6ff66370d1ef5ee360266953bb518fc456e7126c
enrique <enriqueonaga@gmail.com>
2025-03-31 20:59:53 -0300

    crie o serviço para os dados de produtos

--- frontend/src/app/private/services/product.service.ts
+++ frontend/src/app/private/services/product.service.ts
@@ -1,15 +1,15 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, retry, throwError } from 'rxjs';
 import { Environment } from '../../../environment/environment';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiDataService {
+export class ProductService {
 
-  protected urlPrivate = `${Environment.ENV_PRIVATE}`;
-
+  protected urlProduct = `${Environment.ENV_PRODUCT}`;
+  
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   }
@@ -17,7 +17,15 @@ export class ApiDataService {
   constructor(private httpClient: HttpClient) { }
 
   public getUser(): Observable<any> {
-    return this.httpClient.get<any>(`${this.urlPrivate}`)
+    return this.httpClient.get<any>(`${this.urlProduct}`)
+      .pipe(
+        retry(1),
+        catchError(this.handleError)
+      )
+  }
+
+  public postProduct(productData: any): Observable<any> {
+    return this.httpClient.post<any>(this.urlProduct, JSON.stringify(productData), this.httpOptions)
       .pipe(
         retry(1),
         catchError(this.handleError)
--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,5 +1,7 @@
 export enum Environment {
     ENV_LOGIN =          'http://localhost:3000/login',
     ENV_REGISTER =       'http://localhost:3000/register',
-    ENV_PRIVATE =        'http://localhost:3000/private'
+    ENV_PRIVATE =        'http://localhost:3000/private',
+    
+    ENV_PRODUCT =        'http://localhost:3000/product'
 }
